5c167fdb23f2463e24ab33d66d3907af30c6e6d9
BarStaQA <stachowski.bartek@gmail.com>
2021-03-12 10:51:25 +0100

    update code

--- test_store.py
+++ test_store.py
@@ -2,8 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path='C:\TestFiles\chromedriver.exe')
+driver = webdriver.Chrome(executable_path='D:\Quality Assurance Tester\Automation Test Python\PyCharm\chromedriver.exe')
+
 
 driver.get('http://automationpractice.com/index.php')
-search = driver.find_elements_by_id('search_query_top')
-search.send_Keys("dress", Keys.ENTER)
+search_input = driver.find_element_by_name('search_query')
+search_input.send_keys('dress')
+
+search_btn = driver.find_element_by_name('submit_search')
+search_btn.click()
