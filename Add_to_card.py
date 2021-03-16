b195e3d18131c469bad8fa40dfb9adcf4d5b06e5
BarStaQA <stachowski.bartek@gmail.com>
2021-03-16 18:07:35 +0100

    add to card

--- Add_to_card.py
+++ Add_to_card.py
@@ -11,3 +11,9 @@ search_input.send_keys('dress')
 
 search_btn = driver.find_element_by_name('submit_search')
 search_btn.click()
+
+choose_btn = driver.find_element_by_xpath('//*[@id="center_column"]/ul/li[1]/div/div[2]/div[2]/a[1]')
+choose_btn.click()
+
+close_btn = driver.find_element_by_xpath('//*[@id="layer_cart"]/div[1]/div[2]/div[4]/span')
+close_btn.click()
